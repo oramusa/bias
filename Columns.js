d9743833b4fc51a27b3dc9edab0038773e7ebb8a
Alpay Tonga <53499643+oramusa@users.noreply.github.com>
2023-11-04 18:20:16 -0400

    Add files via upload

---
+++ Columns.js
@@ -0,0 +1,178 @@
+export const COLUMNS= [
+
+    {
+        Header:'Data Point',
+        accessor:'data_points'
+    },
+    {
+        Header:'Location',
+        accessor:'location'
+    },
+
+    {
+        Header:'Sale date',
+        accessor:'sale_date'
+    },
+
+    {
+        Header:'Rights',
+        accessor:'rights'
+    },
+
+    {
+Header:'Location Factors',
+accessor:'location_factors'
+    },
+
+    {
+        Header:'Distance to Subject/Miles',
+        accessor:'miles'
+    },
+
+    {
+        Header:'Subject',
+        accessor:'subject1'
+    },
+     {
+        Header:'1 Mile Ring-Median HH income',
+        accessor:'median_income'
+    },
+
+    {
+        Header:'1 Mile Ring-Pop.Density/Sq.Mi',
+        accessor:'pop_density'
+    },
+
+    
+    {
+        Header:'School District',
+        accessor:'school_district'
+    },
+    
+    {
+        Header:'School Rating',
+        accessor:'school_raiting'
+    },
+
+    
+    {
+        Header:'Taxes/Current',
+        accessor:'taxes'
+    },
+
+    
+    {
+        Header:'Traffic Count/Day',
+        accessor:'traffic'
+    },
+
+    {
+        Header:'Physical Factors',
+        accessor:'factors'
+    },
+
+    {
+        Header:'Type',
+        accessor:'type'
+    },
+
+    {
+        Header:'Style',
+        accessor:'style'
+    },
+
+    {
+        Header:'Size',
+        accessor:'size'
+    },
+
+    {
+        Header:'Rooms',
+        accessor:'rooms'
+    },
+
+    {
+        Header:'Bedrooms',
+        accessor:'bedrooms'
+    },
+
+    {
+        Header:'Baths',
+        accessor:'baths'
+    },
+
+    {
+        Header:'Square Footage',
+        accessor:'sqfootage'
+    },
+
+    {
+        Header:'Garage',
+        accessor:'garage'
+    },
+
+    {
+        Header:'Basement',
+        accessor:'basement'
+    },
+
+    {
+        Header:'Finished',
+        accessor:'finished'
+    },
+
+    {
+        Header:'Lot Size/SF',
+        accessor:'lot_size'
+    },
+
+    {
+        Header:'Land to Bldg Ratio',
+        accessor:'ratio'
+    },
+
+    {
+        Header:'Utilities',
+        accessor:'utilities'
+    },
+
+    {
+        Header:'Municipal Water',
+        accessor:'mwater'
+    },
+
+    {
+        Header:'Municipal Sewer',
+        accessor:'msewer'
+    },
+
+    {
+        Header:'Private Well',
+        accessor:'private_well'
+    },
+
+    {
+        Header:'Septic',
+        accessor:'septic'
+    },
+
+    {
+        Header:'Cesspool',
+        accessor:'cesspool'
+    },
+
+    {
+        Header:'Tie Breakers',
+        accessor:'tie_breakers'
+    },
+
+    {
+        Header:'Pool',
+        accessor:'pool'
+    },
+
+    {
+        Header:'Deck',
+        accessor:'deck'
+    },
+]
